5da7ce5a2497d7fb81f09350798543c859bbe045
Alejandro Breen Herrera <31518701+some-tortoise@users.noreply.github.com>
2023-11-10 17:56:16 -0500

    a push with stuff

--- main.py
+++ main.py
@@ -77,33 +77,50 @@ def Dijkstra(graph, source, dest): #added adj list parameter, deleted time varia
   # Initialize distances dictionary with infinity for all vertices except the source
   timeTillPoint = {vertex: float('infinity') for vertex in graph}
   timeTillPoint[sourceNode] = 0
-  i = 0
 
-  priority_queue = [(0, source)]
+  priority_queue = [(sourceNode, 0)]
 
   while priority_queue:
-        current_distance, current_vertex = heapq.heappop(priority_queue)
+        current_vertex, current_distance = heapq.heappop(priority_queue)
         
         # If the current distance is greater than the known distance, skip
         if current_distance > timeTillPoint[current_vertex]:
             continue
-        
+        #print(graph.get(current_vertex))
         # Iterate over neighbors of the current vertex
-        for neighbor, weight in graph[current_vertex].items():
+        print(priority_queue)
+        print(f'ad: {current_vertex}')
+        print(f'b: {graph[current_vertex]}')
+        for neighbor, weight in graph[current_vertex]:
             distance = current_distance + weight
             
             # If a shorter path is found, update the distance
             if distance < timeTillPoint[neighbor]:
                 timeTillPoint[neighbor] = distance
-                heapq.heappush(priority_queue, (distance, neighbor))
-        
-        i += 1
+                print(f'c: {graph[neighbor]}')
+                heapq.heappush(priority_queue, (neighbor, distance))
     
   # Return the time to the destination
-  return timeTillPoint[dest] #this would return distance from given source param to dest param
+  return timeTillPoint[destNode] #this would return distance from given source param to destNode
   
   #return sourceNode, destNode
   
+def getAdjacencyList(dateStr):
+  '''
+  Input:
+  dateString
+  Output:
+  correct adjacencyList
+  '''
+  hour = int(dateStr.split()[1][0:2])
+  adjacencyList = 0
+  date = datetime.strptime(dateStr, "%m/%d/%Y %H:%M:%S")
+  if date.weekday() < 5:
+    adjacencyList = adjacencyListsWeekdays[hour]
+  else:
+    adjacencyList = adjacencyListsWeekends[hour]
+  
+  return adjacencyList
 
 def t1(passengers, drivers):
   '''
@@ -123,30 +140,71 @@ def t1(passengers, drivers):
       passenger = passengers.pop(0)
       waitingPassengerQueue.append(passenger) #add passenger to queue
       if(len(passengers) == 0):
-        print('Ran Out of Passengers')
+        #print('Ran Out of Passengers')
         break
     else:
       driver = drivers.pop(0) 
       waitingDriverQueue.append(driver)
       if(len(drivers) == 0):
-        print('Ran Out of Drivers')
+        #print('Ran Out of Drivers')
         break
     
     if len(waitingDriverQueue) > 0 and len(waitingPassengerQueue) > 0:
       #match passenger to driver
       passenger = waitingPassengerQueue.pop(0)
       driver = waitingDriverQueue.pop(0)
-      #timeFromDriverToPassenger = Djikstra((driver[1], driver[2]), (passenger[1], passenger[2]))
-      #timeFromPassengerToDest = Djikstra((passenger[1], passenger[2]), (passenger[3], passenger[4]))
-      #print(timeFromDriverToPassenger + timeFromPassengerToDest)
 
-      #   totalTime = timeFromDriverToPassenger + timeFromPassengerToDest
-      #   print(f'Total time: {totalTime}')
-        
-      print(Dijkstra((driver[1], passenger[1]), (driver[2], passenger[2]))) #changed driver and passenger pairings
+      passengerDate = datetime.strptime(passenger[0], "%m/%d/%Y %H:%M:%S")
+      driverDate = datetime.strptime(driver[0], "%m/%d/%Y %H:%M:%S")
+
+      #First we want to calculate the hour it is and whether it is a weekday or the weekend
+      #we want to look at the time of the later of the two (driver or passenger) bc that is when the driving will occur
+      #we assume that any trip stays within the same hour segment (even though this is def not true). It makes the adjacency list calculations A GOOD BIT easier
+      #also the average speed from one hour to the next should be relatively similar so our answer should be relatively close
+
+      latestDate = 0
+      if passengerDate < driverDate:
+        latestDate = driver[0]
+      else:
+        latestDate = passenger[0]
+      
+      adjacencyList = getAdjacencyList(latestDate)
+      
+      print(Dijkstra(adjacencyList, (driver[1], driver[2]), (passenger[1], passenger[2])))
 
 
-def createAdjacencyList(type, hour):
+# def createAdjacencyListAsLinkedList(type, hour):
+#   '''
+#   Input:
+#   type can either be 'weekday' or 'weekend'
+#   hour is integer from [0,23]
+
+#   Output: 
+#   adjacency list
+#   '''
+#   adjacencyList = []
+
+#   if(type == 'weekday'):
+#     speedIndex = 3+hour #look at column 3+i for weights
+#   elif(type == 'weekend'):
+#     speedIndex = 27+hour #look at column 27+i for weights
+#   else:
+#     raise TypeError
+  
+
+#   for edge in edges:
+#       speed = float(edge[speedIndex])
+#       length = float(edge[2])
+#       weight = length/speed
+#       if len(adjacencyList) == 0 or adjacencyList[-1][0] != edge[0]:
+#         adjacencyList.append([edge[0],(edge[1], weight)]) # [sourceNode, (destNode, weight)]
+#       else:
+#         adjacencyList[-1].append((edge[1], weight)) # (destNode, weight)
+
+#   return adjacencyList
+
+
+def createAdjacencyListAsDict(type, hour):
   '''
   Input:
   type can either be 'weekday' or 'weekend'
@@ -155,7 +213,7 @@ def createAdjacencyList(type, hour):
   Output: 
   adjacency list
   '''
-  adjacencyList = []
+  adjacencyList = {}
 
   if(type == 'weekday'):
     speedIndex = 3+hour #look at column 3+i for weights
@@ -168,10 +226,10 @@ def createAdjacencyList(type, hour):
       speed = float(edge[speedIndex])
       length = float(edge[2])
       weight = length/speed
-      if len(adjacencyList) == 0 or adjacencyList[-1][0] != edge[0]:
-        adjacencyList.append([edge[0],(edge[1], weight)]) # [sourceNode, (destNode, weight)]
+      if adjacencyList.get(edge[0]):
+        adjacencyList.update({edge[0] : adjacencyList.get(edge[0]).append((edge[1], weight))})
       else:
-        adjacencyList[-1].append((edge[1], weight)) # (destNode, weight)
+        adjacencyList.update({edge[0] : [(edge[1], weight)]})
 
   return adjacencyList
   
@@ -191,16 +249,11 @@ nodes = getNodes()
 # each element in below is for hours from 0-23
 adjacencyListsWeekdays = [0]*24 
 adjacencyListsWeekends = [0]*24 
-for edge in edges:
-  print(edge)
-# for i in range(0,23):
-#   adjacencyListsWeekdays[i] = createAdjacencyList('weekday', i)
-#  adjacencyListsWeekends[i] = createAdjacencyList('weekend', i)
 
 for i in range(0,23):
-  adjacencyListsWeekdays[i] = createAdjacencyList('weekday', i)
-  adjacencyListsWeekends[i] = createAdjacencyList('weekend', i)
+  adjacencyListsWeekdays[i] = createAdjacencyListAsDict('weekday', i)
+  adjacencyListsWeekends[i] = createAdjacencyListAsDict('weekend', i)
 
 # RUN CODE
-#ret = t1(drivers, passengers)
+ret = t1(drivers, passengers)
 #print(ret)
